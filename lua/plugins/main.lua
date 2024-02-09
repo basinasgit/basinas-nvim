0dcb259251f1d50c1718929812f978e0c7a3f79f
basinasgit <157003277+basinasgit@users.noreply.github.com>
2024-02-09 14:52:19 +0200

    add catpuccin as the default theme

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -1,12 +1,12 @@
 return {
   -- add rose-pine-moon theme
-  { "rose-pine/neovim" },
+  { "catppuccin/nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine-moon",
+      colorscheme = "catppuccin",
     },
   },
 }
